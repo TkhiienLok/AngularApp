a952ab40bd1870f23a243c0a90d196352491d928
TkhiienLok <tkhiien@gmail.com>
2019-06-21 15:59:13 +0300

    Corrected server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/get-user-data'));
+app.use(express.static('./dist/angular-httpclient-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/get-user-data/index.html'));
+res.sendFile(path.join(__dirname,'/dist/angular-httpclient-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
